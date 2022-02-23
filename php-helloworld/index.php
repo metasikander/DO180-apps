3e29bea811f23386d2f96b42e929926098bac421
sikander <sikander@lhote.me>
2022-02-23 17:10:40 +0100

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This is the console version!\n"
+print "This is the console version!\n";
+print "A change is in the air!\n";
 ?>
